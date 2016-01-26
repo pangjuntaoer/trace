e3c94d31e182002e821a901fdd62fdfe759c9ba7
Administrator <Administrator@C64-20141219CPG>
2016-01-26 17:42:41 +0800

    db trace

---
+++ trace-db/src/main/java/com/zxq/iov/cloud/trace/db/App.java
@@ -0,0 +1,13 @@
+package com.zxq.iov.cloud.trace.db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
