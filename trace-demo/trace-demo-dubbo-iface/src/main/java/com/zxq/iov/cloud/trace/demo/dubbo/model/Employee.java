9500bf872dc7ab53215c5491c23c09ebe984f6ec
Administrator <Administrator@C64-20141219CPG>
2016-03-17 16:18:50 +0800

    job

--- trace-demo/trace-demo-dubbo-iface/src/main/java/com/zxq/iov/cloud/trace/demo/dubbo/model/Employee.java
+++ trace-demo/trace-demo-dubbo-iface/src/main/java/com/zxq/iov/cloud/trace/demo/dubbo/model/Employee.java
@@ -7,9 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @SuppressWarnings("serial")
-@Entity(name = "employee")
+@Entity
+@Table(name = "employee")
 public class Employee extends BaseEntity implements Serializable {
 	
 	@Id
