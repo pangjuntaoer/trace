a27c80187fde575e55376fbc58912711c0e38f55
Administrator <Administrator@C64-20141219CPG>
2016-05-03 12:26:32 +0800

    rename trace-demo-dubbo

---
+++ trace-demo/trace-demo-dubbo/src/main/java/org/trace/demo/dubbo/App.java
@@ -0,0 +1,13 @@
+package org.trace.demo.dubbo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ trace-demo/trace-demo-dubbo/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello trace demo dubbo provider!</h2>
+</body>
+</html>
